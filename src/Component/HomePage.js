bd079ab753cb786fc0df0e246d210dbca15191c6
Yaroslav-git <kuvyatkin@gmail.com>
2021-10-18 22:28:14 +0300

    Версия с function-component'ами и react-router. (без Redux)

---
+++ src/Component/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg'
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Книжная База Данных</h1>
+            <img src={logo} height="768" alt="react logo"/>
+        </div>
+    )
+};
+
+export default HomePage
